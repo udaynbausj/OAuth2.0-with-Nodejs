2a1dfbbf09944d649734f00c144e1c25712ef3c9
uday <uday@Uday-BK.local>
2019-03-16 18:44:05 +0530

    successfully saved User info in MongoDB Atlas

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,6 +1,7 @@
 const passport = require('passport');
 const google_strategy = require('passport-google-oauth20');
 const keys = require('./keys');
+const UserModel = require("../Repository/user_model");
 
 passport.use(
     new google_strategy({
@@ -11,6 +12,14 @@ passport.use(
     }, (accessToken,refreshToken,profile,done) => {
         console.log('Callback fired');
         console.log(profile);
-
+        let googleProfile = new UserModel({
+            username : profile.displayName,
+            googleId : profile.id
+        });
+        googleProfile.save().then( (resolvedResult) =>{
+            console.log("Successfuly saved to DB");
+        }, (err) => {
+            console.log("Error occured in saving to DB");
+        });
     })
 );
